82e15e26bacfe4cfeb4547b83050454945c176f9
Tomasz Kardas <tomasz.kardas@strix.net>
2022-04-12 10:33:09 +0200

    use language for too long exception

--- src/Infrastructure/Exception/Mapper/ProductToLongValueException.php
+++ src/Infrastructure/Exception/Mapper/ProductToLongValueException.php
@@ -15,7 +15,7 @@ use Ergonode\Product\Domain\ValueObject\Sku;
 
 class ProductToLongValueException extends Shopware6ExporterException
 {
-    private const MESSAGE = 'Attribute {code} is too long max {length}, required for product {sku}';
+    private const MESSAGE = 'Attribute {code} is too long max {length}, required for product {sku} language {language}';
 
     public function __construct(AttributeCode $code, Sku $sku, int $length, Language $language, \Throwable $previous = null)
     {

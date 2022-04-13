633dc3e5b6424997ae2c69411be1e854c2058435
Tomasz Kardas <tomasz.kardas@strix.net>
2022-04-13 11:44:18 +0200

    file mapper

---
+++ src/Infrastructure/Mapper/Product/CustomField/ProductCustomFieldSetFileMapper.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright © Ergonode Sp. z o.o. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Ergonode\ExporterShopware6\Infrastructure\Mapper\Product\CustomField;
+
+use Ergonode\Attribute\Domain\Entity\AbstractAttribute;
+use Ergonode\Attribute\Domain\Entity\Attribute\AbstractImageAttribute;
+use Ergonode\Attribute\Domain\Entity\Attribute\FileAttribute;
+use Ergonode\Attribute\Domain\Repository\AttributeRepositoryInterface;
+use Ergonode\ExporterShopware6\Domain\Entity\Shopware6Channel;
+use Ergonode\ExporterShopware6\Infrastructure\Client\Shopware6ProductMediaClient;
+use Ergonode\ExporterShopware6\Infrastructure\Exception\Mapper\Shopware6ExporterMultimediaException;
+use Ergonode\ExporterShopware6\Infrastructure\Mapper\Product\AbstractProductCustomFieldSetMapper;
+use Ergonode\ExporterShopware6\Infrastructure\Model\Shopware6Product;
+use Ergonode\Multimedia\Domain\Repository\MultimediaRepositoryInterface;
+use Ergonode\Product\Infrastructure\Calculator\TranslationInheritanceCalculator;
+use Ergonode\SharedKernel\Domain\Aggregate\MultimediaId;
+
+class ProductCustomFieldSetFileMapper extends AbstractProductCustomFieldSetMapper
+{
+    private MultimediaRepositoryInterface $multimediaRepository;
+
+    private Shopware6ProductMediaClient $mediaClient;
+
+    public function __construct(
+        AttributeRepositoryInterface $repository,
+        TranslationInheritanceCalculator $calculator,
+        MultimediaRepositoryInterface $multimediaRepository,
+        Shopware6ProductMediaClient $mediaClient
+    ) {
+        parent::__construct($repository, $calculator);
+        $this->multimediaRepository = $multimediaRepository;
+        $this->mediaClient = $mediaClient;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getType(): string
+    {
+        return FileAttribute::TYPE;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws Shopware6ExporterMultimediaException
+     */
+    protected function getValue(
+        Shopware6Channel $channel,
+        AbstractAttribute $attribute,
+        $calculateValue,
+        Shopware6Product $shopware6Product = null
+    ): string {
+        $multimediaId = new MultimediaId($calculateValue);
+
+        return $this->getShopware6MultimediaId($channel, $multimediaId, $shopware6Product, $attribute);
+    }
+
+    /**
+     * @throws Shopware6ExporterMultimediaException
+     */
+    private function getShopware6MultimediaId(
+        Shopware6Channel $channel,
+        MultimediaId $multimediaId,
+        Shopware6Product $shopware6Product,
+        AbstractAttribute $attribute
+    ): string {
+        $multimedia = $this->multimediaRepository->load($multimediaId);
+        if ($multimedia) {
+            return $this->mediaClient->findOrCreateMedia($channel, $multimedia, $shopware6Product, $attribute);
+        }
+        throw new Shopware6ExporterMultimediaException($multimediaId);
+    }
+}
